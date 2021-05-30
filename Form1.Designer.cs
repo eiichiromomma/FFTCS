3517d1582f8928f32f182485458bb1ee519498ed
Eiichiro Momma <mommaeiichiro@outlook.com>
2021-05-30 20:36:55 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FFTCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.CAMpictureBox = new System.Windows.Forms.PictureBox();
+            this.IFFTpictureBox = new System.Windows.Forms.PictureBox();
+            this.FFTpictureBox = new System.Windows.Forms.PictureBox();
+            this.FILTERpictureBox = new System.Windows.Forms.PictureBox();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CAMpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IFFTpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FFTpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FILTERpictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.CAMpictureBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.IFFTpictureBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.FFTpictureBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.FILTERpictureBox, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(626, 453);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // CAMpictureBox
+            // 
+            this.CAMpictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CAMpictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CAMpictureBox.Location = new System.Drawing.Point(3, 3);
+            this.CAMpictureBox.Name = "CAMpictureBox";
+            this.CAMpictureBox.Size = new System.Drawing.Size(307, 220);
+            this.CAMpictureBox.TabIndex = 0;
+            this.CAMpictureBox.TabStop = false;
+            // 
+            // IFFTpictureBox
+            // 
+            this.IFFTpictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.IFFTpictureBox.Location = new System.Drawing.Point(316, 3);
+            this.IFFTpictureBox.Name = "IFFTpictureBox";
+            this.IFFTpictureBox.Size = new System.Drawing.Size(307, 220);
+            this.IFFTpictureBox.TabIndex = 1;
+            this.IFFTpictureBox.TabStop = false;
+            // 
+            // FFTpictureBox
+            // 
+            this.FFTpictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FFTpictureBox.Location = new System.Drawing.Point(3, 229);
+            this.FFTpictureBox.Name = "FFTpictureBox";
+            this.FFTpictureBox.Size = new System.Drawing.Size(307, 221);
+            this.FFTpictureBox.TabIndex = 2;
+            this.FFTpictureBox.TabStop = false;
+            // 
+            // FILTERpictureBox
+            // 
+            this.FILTERpictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.FILTERpictureBox.BackColor = System.Drawing.Color.Black;
+            this.FILTERpictureBox.Location = new System.Drawing.Point(316, 229);
+            this.FILTERpictureBox.Name = "FILTERpictureBox";
+            this.FILTERpictureBox.Size = new System.Drawing.Size(307, 221);
+            this.FILTERpictureBox.TabIndex = 3;
+            this.FILTERpictureBox.TabStop = false;
+            this.FILTERpictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.FILTERpictureBox_Paint);
+            this.FILTERpictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FILTERpictureBox_MouseDown);
+            this.FILTERpictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FILTERpictureBox_MouseMove);
+            this.FILTERpictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FILTERpictureBox_MouseUp);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Clearbutton.Location = new System.Drawing.Point(12, 471);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(114, 46);
+            this.Clearbutton.TabIndex = 1;
+            this.Clearbutton.Text = "Clear";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 529);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CAMpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IFFTpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FFTpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FILTERpictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox CAMpictureBox;
+        private System.Windows.Forms.PictureBox IFFTpictureBox;
+        private System.Windows.Forms.PictureBox FFTpictureBox;
+        private System.Windows.Forms.PictureBox FILTERpictureBox;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace FFTCS
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture cap;
+        List<System.Drawing.Point> pts;
+        Graphics CAMgraphics;
+        Graphics FFTgraphics;
+        Graphics IFFTgraphics;
+        Graphics FILTERgraphics;
+        Stack<List<System.Drawing.Point>> Strokes;
+        Mat filter;
+        Pen wpen;
+        Pen drawPen;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Mat mulChecker(Mat m)
+        {
+            for (int y = 0; y < m.Rows; y++)
+            {
+                for (int x = 0; x < m.Cols; x++)
+                {
+                    m.Set(y, x, m.Get<double>(y, x) * Math.Pow(-1, x + y));
+                }
+            }
+            return m;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Mat frame = new Mat();
+            Mat tmpImg = new Mat();
+            Mat gframe = new Mat();
+            Mat padded = new Mat();
+            Mat padded32 = new Mat();
+            cap.Read(frame);
+
+            //表示
+            var dy = (float)(CAMpictureBox.Height) / frame.Height;
+            tmpImg = frame.Resize(new OpenCvSharp.Size(frame.Width * dy, CAMpictureBox.Height));
+            Bitmap tmpBitmap = BitmapConverter.ToBitmap(tmpImg);
+            CAMgraphics.DrawImage(tmpBitmap, 0, 0, tmpImg.Width, tmpImg.Height);
+
+            Cv2.CvtColor(frame, gframe, ColorConversionCodes.BGR2GRAY);
+            var m = Cv2.GetOptimalDFTSize(gframe.Rows);
+            var n = Cv2.GetOptimalDFTSize(gframe.Cols);
+            Cv2.CopyMakeBorder(gframe, padded, 0, m - gframe.Rows, 0, n - gframe.Cols, BorderTypes.Constant, Scalar.All(0));
+            padded.ConvertTo(padded32, MatType.CV_32F, 1 / 255.0, 0);
+            //Mulを使うとメモリをガンガン使って不安になるので自前で-1^(x+y)をやる
+            padded32 = mulChecker(padded32);
+
+            // フーリエ
+            Mat tmpZero = new Mat(padded32.Size(), MatType.CV_32F);
+            Mat dft = new Mat();
+            Mat[] complex = { padded32, tmpZero };
+            Mat mergedComplex = new Mat();
+            Cv2.Merge(complex, mergedComplex);
+            Cv2.Dft(mergedComplex, dft, DftFlags.None, 0);
+            // 0:実部，1:虚部なのでSplitする
+            Mat[] dftplanes = Cv2.Split(dft);
+            Mat magnitude = new Mat();
+            Cv2.Magnitude(dftplanes[0], dftplanes[1], magnitude);
+            // 1加算してからLogで見易く
+            magnitude += Scalar.All(1);
+            Cv2.Log(magnitude, magnitude);
+            Cv2.Normalize(magnitude, magnitude, 0, 1, NormTypes.MinMax);
+            // 表示用uint8
+            Mat magnitude8 = new Mat();
+            Cv2.ConvertScaleAbs(magnitude, magnitude8, 255, 0);
+            tmpImg = magnitude8.Resize(new OpenCvSharp.Size(frame.Width * dy, CAMpictureBox.Height));
+            tmpBitmap = BitmapConverter.ToBitmap(tmpImg);
+            FFTgraphics.DrawImage(tmpBitmap, 0, 0, tmpBitmap.Width, tmpBitmap.Height);
+
+            // 3チャネルなのでSplitして1枚だけ使う
+            Mat[] filterplanes = Cv2.Split(filter);
+            Mat dfilter = new Mat(filterplanes[1].Size(), MatType.CV_32F);
+            filterplanes[1].ConvertTo(dfilter, MatType.CV_32F, 1 / 255.0, 0);
+            dfilter = dfilter.Resize(frame.Size());
+            dftplanes[0] = dftplanes[0].Mul(dfilter);
+            dftplanes[1] = dftplanes[1].Mul(dfilter);
+
+            // 逆フーリエ
+            Cv2.Merge(dftplanes, dft);
+            Mat invdft = new Mat();
+            Cv2.Dft(dft, invdft, DftFlags.Inverse | DftFlags.RealOutput);
+            invdft = mulChecker(invdft);
+            Cv2.Normalize(invdft, invdft, 0, 1, NormTypes.MinMax);
+            Mat invdft8 = new Mat();
+            Cv2.ConvertScaleAbs(invdft, invdft8, 255, 0);
+            tmpImg = invdft8.Resize(new OpenCvSharp.Size(frame.Width * dy, CAMpictureBox.Height));
+            tmpBitmap = BitmapConverter.ToBitmap(tmpImg);
+            IFFTgraphics.DrawImage(tmpBitmap, 0, 0, tmpBitmap.Width, tmpBitmap.Height);
+            
+            //不要だがメモリをモリモリ使うのでゴミ捨て
+            invdft8.Dispose();
+            invdft.Dispose();
+            magnitude8.Dispose();
+            magnitude.Dispose();
+            mergedComplex.Dispose();
+            dft.Dispose();
+            tmpZero.Dispose();
+            gframe.Dispose();
+            padded.Dispose();
+            padded32.Dispose();
+            frame.Dispose();
+            tmpImg.Dispose();
+            tmpBitmap.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cap = new VideoCapture(0);
+            pts = new List<System.Drawing.Point>();
+            wpen = new Pen(Color.White);
+            drawPen = new Pen(Color.White, 5.0f);
+            Strokes = new Stack<List<System.Drawing.Point>>();
+            //pictureBoxごとにGraphicsを取得してDrawImageしないとメモリを食い潰す
+            CAMgraphics = CAMpictureBox.CreateGraphics();
+            FFTgraphics = FFTpictureBox.CreateGraphics();
+            IFFTgraphics = IFFTpictureBox.CreateGraphics();
+
+            Mat frame = new Mat();
+            cap.Read(frame);
+            // カメラによって画像サイズやアスペクトが不明なので，1枚取得してそれに合わせる
+            var dy = (float)(CAMpictureBox.Height) / frame.Height;
+            FILTERpictureBox.Size = new System.Drawing.Size((int)(frame.Width * dy), CAMpictureBox.Height);
+            FILTERgraphics = FILTERpictureBox.CreateGraphics();
+            filter = Mat.Zeros(new OpenCvSharp.Size(frame.Width * dy, CAMpictureBox.Height), MatType.CV_8UC3);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            cap.Release();
+        }
+
+        private void FILTERpictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            // →参照 https://www.umayadia.com/cssample/sample0201/Sample275WinFormMouseToDraw.htm
+            Strokes.Push(new List<System.Drawing.Point>());
+        }
+
+        private void FILTERpictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            // →参照 https://www.umayadia.com/cssample/sample0201/Sample275WinFormMouseToDraw.htm
+            foreach (var stroke in Strokes)
+            {
+                var path = new System.Drawing.Drawing2D.GraphicsPath(stroke.ToArray(), Enumerable.Repeat<byte>(1, stroke.Count).ToArray());
+                e.Graphics.DrawPath(drawPen, path);
+            }
+        }
+
+        private void FILTERpictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            // →参照 https://www.umayadia.com/cssample/sample0201/Sample275WinFormMouseToDraw.htm
+            if (Control.MouseButtons != MouseButtons.None)
+            {
+                Strokes.Peek().Add(e.Location);
+                FILTERpictureBox.Invalidate();
+            }
+        }
+
+        private void FILTERpictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            //マウスのドラッグが終わった時点でビットマップに画面と同じものを書き込んでMatに変換
+            Bitmap filterBmp = new Bitmap(FILTERpictureBox.Width, FILTERpictureBox.Height);
+            Graphics g = Graphics.FromImage(filterBmp);
+            foreach (var stroke in Strokes)
+            {
+                var path = new System.Drawing.Drawing2D.GraphicsPath(stroke.ToArray(), Enumerable.Repeat<byte>(1, stroke.Count).ToArray());
+                g.DrawPath(drawPen, path);
+            }
+            filter = BitmapConverter.ToMat(filterBmp);
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            pts.Clear();
+            Strokes.Clear();
+            FILTERgraphics.Clear(Color.Black);
+            filter.SetTo(0);
+        }
+    }
+}
